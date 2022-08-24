20eb7a19bd573677b1264b490932db62e82dece5
MisterLs4566 <lkronenb1@googlemail.com>
2022-08-25 00:23:53 +0200

    update

--- Assets/Scripts/General/PlayerController.cs
+++ Assets/Scripts/General/PlayerController.cs
@@ -4,35 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed, runSpeed, hearts;
+    public float speed, runSpeed, hearts, knockback;
     private float currentSpeed, angle;
     private float horizontalInput, verticalInput;
+    private float maxHearts, knockbackVelocity;
     private CharacterController playerC;
+    private Animator playerAnimator;
+    private Rigidbody playerRb;
     private Vector3 velocity, mousePos, playerPos;
     private Vector2 velocity2d;
-    private Animator playerAnimator;
-    private bool isShooting;
+    private bool isShooting, isWaitingKnockback;
     private string weapon;
+    private bool receiveDamage;
+    private Vector3 playerDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        maxHearts = hearts;
         currentSpeed = speed;
         isShooting = false;
+        receiveDamage = true;
+        isWaitingKnockback = false;
         weapon = "";
 
         playerC = GetComponent<CharacterController>();
         playerAnimator = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementInput();
+        if(receiveDamage == true)
+        {
+            movementInput();
+        }
+        else
+        {
+            horizontalInput = 0;
+            verticalInput = 0;  
+        }
         Move();
-        Rotate();
+        if(receiveDamage == true)
+        {
+            Rotate();
+        }
         Shoot();
         Animate();
+        Knockback();
     }
 
     void movementInput()
@@ -115,11 +135,74 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision other)
+    void Knockback()
+    {
+        if (knockbackVelocity != 0)
+        {
+            if(knockbackVelocity < 0)
+            {
+                if(isWaitingKnockback == false)
+                {
+                    StartCoroutine(waitDamage());
+                }
+            }
+            else
+            {
+                knockbackVelocity = 0f;
+            }
+        }
+    }
+
+    IEnumerator getDamage()
+    {
+        playerAnimator.SetBool("getDamage", true);
+        playerAnimator.SetBool("isRunning", false);
+        hearts -= 10;
+        receiveDamage = false;
+        knockbackVelocity = -knockback;
+        yield return new WaitForSeconds(1);
+        receiveDamage = true;
+        playerAnimator.SetBool("getDamage", false);
+    }
+
+    IEnumerator waitDamage()
+    {
+        isWaitingKnockback = true;
+        yield return new WaitForSeconds(0.05f);
+        velocity += playerDirection * knockbackVelocity;
+        knockbackVelocity += 1.5f;
+        isWaitingKnockback = false;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit other)
     {
         if(other.gameObject.layer == 6)
         {
+            if(other.gameObject.CompareTag("Food"))
+            {
+                hearts += 25;
+            }
+
             Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Zombie"))
+        {
+            if(receiveDamage == true)
+            {
+                playerDirection = gameObject.transform.up;
+                velocity = new Vector3(0, 0, 0);
+                StartCoroutine(getDamage());
+            }
+        }
+
+        if (hearts > maxHearts)
+        {
+            hearts = maxHearts;
+        }
+        else if (hearts <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
